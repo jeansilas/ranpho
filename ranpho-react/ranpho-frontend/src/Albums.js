9fef128ddfcdd8131b8a6c9111c60e68ff4b6e3e
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2021-11-08 17:14:25 -0300

    menu customizado

--- ranpho-react/ranpho-frontend/src/Albums.js
+++ ranpho-react/ranpho-frontend/src/Albums.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Album from './components/Album';
+import "./Albums.css";
 
 export default function Albums() {
-   return (
-    <div>     
-    
+  const [albumName, setAlbumName] = useState([]);
+
+  const loadData = () => {
+    axios.get("http://127.0.0.1:8000/api/albums/").then(
+      (response) => {
+        setAlbumName(response.data);
+        console.log(response.data)
+      });
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+  
+  
+  return (
+    <div className="Body-albums">     
       <div className="Buttons-bar">
         <div className="img-text">
           <img className="logo" src="/love.png" alt="logo"/>
@@ -12,12 +30,13 @@ export default function Albums() {
       </div>
 
       <div className="Albums">
-
-        
+        {
+          albumName.map(
+            (album) => (
+              <Album key={`album__${album.id}`} src={album.title}></Album> 
+            ))
+        }
       </div>
-
-
-
     </div>
     );
 }
--- ranpho-react/ranpho-frontend/src/components/Album/index.js
+++ ranpho-react/ranpho-frontend/src/components/Album/index.js
@@ -1,43 +1,41 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
+import { Link } from 'react-router-dom';
 import axios from "axios";
 
 
-export default function Foto(props) {
-  const [titleAlbum, setTitleAlbum] = useState('');
+export default function Album(props) {
+  const [color, setColor] = useState("");
 
-  const titleChanged = (event) => {
-    setTitleAlbum(event.target.value);
+  const getColor = () => {
+    let arr = ["#CABAC8", "#FFCB77", "#92EBCD", "#95D9DA", "#E5A4CB", "#037971", "#EFC88B", "#EFAAC4"];
+    setColor(arr[Math.floor(Math.random() * arr.length)]);
+    console.log(color)
   };
 
-  const saveImg = (event) => {
-    event.preventDefault();
-    axios.post("http://localhost:8000/api/pics/", {album:titleAlbum, content:props.src}).then(
-        (response) => {
-          setTitleAlbum("");
-        });
-  };
+  useEffect(() => {
+    getColor();
+  }, []);
+
+  const style = {backgroundColor: `${color}`};
 
   return (
-    <div className="contornoImagem">
-      <img className="img" alt="Doguinho" src={props.src}>
-      </img>
-    
-      <form className="forms" onSubmit={saveImg}>
-        <input
-            className="title-album"
-            type="text"
-            name="titulo"
-            placeholder="Insira um nome"
-            value={titleAlbum}
-            onChange={titleChanged}
-        />
+    <div className="album-card">
+     
+      <div className="contorno-album">
         
-        <button className="send-button" type="submit">
-          <img className="send" src="/heart.png" alt="enviar"/>
-        </button>
-      </form>
+        <div className="album-content"> 
+          <div className="album-color" style={style}>  
+                
+          </div>          
+          <div className="link">
+            <Link className="album-title" to="/">{props.src}</Link>
+          </div>
+        </div>
+      
+      </div>
+    
     </div>
   );
 }
